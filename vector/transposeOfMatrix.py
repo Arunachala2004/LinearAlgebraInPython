89b673310ee216e3f617a6eb1e621a140c667a25
Arunachala <93348029+Arunachala2004@users.noreply.github.com>
2024-01-06 11:42:54 +0530

    Transpose Of a matrix Using Matrix

---
+++ vector/transposeOfMatrix.py
@@ -0,0 +1,6 @@
+import numpy as np #importing required libraries (numpy as np)
+A=np.array([[1,2],[3,4],[5,6]])#Creating a 3X2 matrix "A"
+print("Matrix A: \n", A)
+
+#finding the transpose of a matrix using the function transpose()
+print("Matrix A transpose:\n", np.transpose(A))
