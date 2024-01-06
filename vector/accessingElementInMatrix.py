de793d8e3bce13be1346679cc4008b205a95670a
Arunachala <93348029+Arunachala2004@users.noreply.github.com>
2024-01-06 11:32:40 +0530

    Accessing the matrix element using the index.

---
+++ vector/accessingElementInMatrix.py
@@ -0,0 +1,4 @@
+import numpy as np #importing the required libraries (numpy as np)
+#creating a 3X3 matrix
+A=np.array([[1,2],[4,5],[6,7]])#in python idexing starts from zero....
+print("Element of matrix A at position [2,1] is",A[2,1])
